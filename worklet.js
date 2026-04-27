66415f92d51c7a0c4da35c5ecd594dfb5057c00b
Nachos <avankimbleton270@gmail.com>
2026-04-26 20:57:50 -0400

    Rename Bytebeat class to BytebeatProcessor and refactor

--- worklet.js
+++ worklet.js
@@ -1,20 +1,33 @@
-class Bytebeat extends AudioWorkletProcessor {
+class BytebeatProcessor extends AudioWorkletProcessor {
   constructor() {
     super();
 
     this.t = 0;
     this.step = 1;
+    this.rand = 1;
 
-    this.fn = (t) => 0;
+    // PERSISTENT GLOBAL STATE (THIS IS THE FIX)
+    this.env = {
+      ec: new Array(12288).fill(0),
+      A: [],
+      n: 12288,
+      random: () => {
+        this.rand = (this.rand * 16807) % 2147483647;
+        return this.rand / 2147483647;
+      }
+    };
+
+    this.fn = null;
 
     this.port.onmessage = (e) => {
       if (e.data.reset) {
         this.t = 0;
+        this.env.ec.fill(0);
         return;
       }
 
-      if (e.data.step) {
-        this.step = e.data.step;
+      if (e.data.rate) {
+        this.step = e.data.rate / sampleRate;
       }
 
       if (e.data.fn) {
@@ -24,31 +37,31 @@ class Bytebeat extends AudioWorkletProcessor {
   }
 
   process(_, outputs) {
-    const outL = outputs[0][0];
-    const outR = outputs[0][1] || outL;
+    const L = outputs[0][0];
+    const R = outputs[0][1] || L;
 
-    for (let i = 0; i < outL.length; i++) {
+    for (let i = 0; i < L.length; i++) {
       let v = 0;
 
       try {
-        v = this.fn(this.t);
+        v = this.fn(this.t, this.env);
       } catch {
         v = 0;
       }
 
-      let l = v, r = v;
+      let l = 0, r = 0;
 
       if (Array.isArray(v)) {
-        l = v[0];
-        r = v[1];
+        [l, r] = v;
+      } else {
+        l = r = v;
       }
 
-      // normalize
       if (l > 1 || l < -1) l = ((l & 255) / 128) - 1;
       if (r > 1 || r < -1) r = ((r & 255) / 128) - 1;
 
-      outL[i] = l;
-      outR[i] = r;
+      L[i] = l;
+      R[i] = r;
 
       this.t += this.step;
     }
@@ -57,4 +70,4 @@ class Bytebeat extends AudioWorkletProcessor {
   }
 }
 
-registerProcessor("bytebeat", Bytebeat);
+registerProcessor("bytebeat", BytebeatProcessor);
