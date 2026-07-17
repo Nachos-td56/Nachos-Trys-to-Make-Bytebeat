71b3fcf0dabf5e25ee7b126d44428c38f65d51ec
Nachos <avankimbleton270@gmail.com>
2026-07-17 09:22:43 -0400

    Update app.js

--- app.js
+++ app.js
@@ -216,7 +216,7 @@ document.getElementById('play').onclick = async () => {
                   pow=Math.pow, random=Math.random, round=Math.round, sign=Math.sign, sin=Math.sin,
                   sinh=Math.sinh, sqrt=Math.sqrt, tan=Math.tan, tanh=Math.tanh, trunc=Math.trunc,
                   pi=Math.PI, PI=Math.PI;
-            let b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,u,v,x,y,z;
+            let a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z;
         `;
 
         const helper = styleSelect.value === 'simple' ? memoryHelper + mathHelper : memoryHelper;
